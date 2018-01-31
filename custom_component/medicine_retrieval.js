3fd29e11c356a0913dc697f8b84e39997c5b6790
MinjungKim <alswnd0427@gmail.com>
2018-01-31 11:19:12 +0900

    데이터 null 일때 예외처리

--- custom_component/medicine_retrieval.js
+++ custom_component/medicine_retrieval.js
@@ -13,26 +13,28 @@ var pool = mysql.createPool({
     database: 'medicine_test'
 });
 
+// array for searched medicine's info
 var medicine_name = [];
 
 function hitQuery(GeneralMedicineName) {
     return new Promise((resolve, reject) => {
-        // connection.connect();
         var sql = 'SELECT * FROM medicine_list WHERE generalname like "' + GeneralMedicineName + '%"';
+
         pool.query(sql, (err, rows) => {
-
-            medicine_name = rows;
-            resolve();
-            // }
-
+            // when data is null
+            if (err) {
+                reject(new Error(err));
+            }
+            // when data is not null
+            else {
+                medicine_name = rows;
+                resolve();
+            }
         });
-        // connection.release();
     });
-
 }
 
-var test = [];
-function hitQueryReturnRows(GeneralMedicineName, callback) {
+/* function hitQueryReturnRows(GeneralMedicineName, callback) {
     pool.query('SELECT * FROM medicine_list WHERE generalname = "' + GeneralMedicineName + '"', (err, rows) => {
         if (err)
             callback(err, null);
@@ -42,6 +44,12 @@ function hitQueryReturnRows(GeneralMedicineName, callback) {
     });
 }
 
+console.log('----------------------------------------------------------------------------------------------------------');
+var _rows = hitQueryReturnRows(GeneralMedicineName, (err, rows) => {
+    _rows = rows;
+});
+console.log(_rows);
+console.log('----------------------------------------------------------------------------------------------------------'); */
 
 
 module.exports = {
@@ -55,73 +63,61 @@ module.exports = {
     }),
 
     invoke: (conversation, done) => {
-
         var InputMedicineName = conversation.messagePayload().text;
         var GeneralMedicineName = hangul.hanguler(InputMedicineName);
 
-        // console.log('----------------------------------------------------------------------------------------------------------');
-        var _rows = hitQueryReturnRows(GeneralMedicineName, (err, rows) => {
-            _rows = rows;
-        });
-        console.log(_rows);
-        // console.log('----------------------------------------------------------------------------------------------------------');
-
-
         var promise = hitQuery(GeneralMedicineName).then(() => {
-            if (medicine_name == 'no data') {
-                conversation.reply({ text: 'no data' });
+            var name = medicine_name[0].name;
+            var efficacy = medicine_name[0].efficacy;
+            var howToUse = medicine_name[0].howtouse;
+            var precaution = medicine_name[0].precaution;
+            var originalUrl = medicine_name[0].originalurl;
+            var imgUrl = medicine_name[0].imgurl;
+
+            var _attachment = {
+                type: 'image',
+                url: imgUrl
             }
-            else {
-                var name = medicine_name[0].name;
-                var efficacy = medicine_name[0].efficacy;
-                var howToUse = medicine_name[0].howtouse;
-                var precaution = medicine_name[0].precaution;
-                var originalUrl = medicine_name[0].originalurl;
-                var imgUrl = medicine_name[0].imgurl;
+            // reply in facebook
+            conversation.reply({
+                text: '약이름 : ' + name
+            });
+            conversation.reply({
+                type: 'attachment',
+                attachment: _attachment
+            });
+            conversation.reply({
+                text: '효능 : ' + efficacy
+            });
+            conversation.reply({
+                text: '용법 : ' + howToUse
+            });
+            conversation.reply({
+                text: '주의사항 : ' + precaution
+            });
+            conversation.reply({
+                text: '자세한 정보 : ' + originalUrl
+            });
 
-                var _attachment = {
-                    type: 'image',
-                    url: imgUrl
-                }
-                // reply in facebook
-                conversation.reply({
-                    text: '약이름 : ' + name
-                });
-                conversation.reply({
-                    type: 'attachment',
-                    attachment: _attachment
-                });
-                conversation.reply({
-                    text: '효능 : ' + efficacy
-                });
-                conversation.reply({
-                    text: '용법 : ' + howToUse
-                });
-                conversation.reply({
-                    text: '주의사항 : ' + precaution
-                });
-                conversation.reply({
-                    text: '자세한 정보 : ' + originalUrl
-                });
-
-                // test in kakaotalk -> success
-                // conversation.reply({
-                //     message: {
-                //         text: '약이름 : ' + name,
-                //         photo: { url: '' + imgUrl, width: 640, height: 480 }
-                //     }
-                // });
-
-                conversation.transition();
-
-                done();
-
-            }
-
+            conversation.transition();
+            done();
         }).catch(err => {
-            reject(err);
+            conversation.reply({
+                text: '해당하는 약이 없습니다.'
+            });
+
+            conversation.transition();
+            done();
         });
     }
-
 };
 
+
+
+/* // test in kakaotalk -> success
+conversation.reply({
+    message: {
+        text: '약이름 : ' + name,
+        photo: { url: '' + imgUrl, width: 640, height: 480 }
+    }
+}); */
