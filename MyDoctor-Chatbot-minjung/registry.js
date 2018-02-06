95b0222069979ce14b1a0216b0ee5dcf232e2036
MinjungKim <alswnd0427@gmail.com>
2018-02-06 14:35:02 +0900

    잘못된 파일 삭제

--- MyDoctor-Chatbot-minjung/registry.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  components: {
-    //custom component registry
-    'MedicineRetrieval' : require('./custom_component/medicine_retrieval'),
-    'MakeReservation' : require('./custom_component/make_reservation')
-    // 'Replier' : require('./custom_component/replier')
-  }
-};
