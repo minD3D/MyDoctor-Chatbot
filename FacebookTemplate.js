c2ec33dfcfe596fed281c13206f8c1192988d0a2
[minD3D] <gomdori626@gmail.com>
2018-02-07 14:36:26 +0900

    add hj resource

--- FacebookTemplate.js
+++ FacebookTemplate.js
@@ -1,10 +1,26 @@
 "use strict";
 var express = require("express");
+var path = require('path');
 module.exports= {
+
+
 genericFBT: function genericFBT(genericImgUrl, genericTitile, genericSubtitile, genericButtonName, genericButtonURL ){
   //입력된 부분을 generic 템플릿 형식으로 변환
 
   function genericTemplate(imgurl, title, subtitle, buttonname, buttonurl){
+    
+    // var arSplitUrl = imgurl.split("/");    // "/" 로 전체 url 을 나눈다
+    // var nArLength = arSplitUrl.length;
+    // var arFileName = arSplitUrl[nArLength-1];   // 나누어진 배열의 맨 끝이 파일명이다
+    // var arSplitFileName = arFileName.split(".");   // 파일명을 다시 "." 로 나누면 파일이름과 확장자로 나뉜다
+    // var sFileName = arSplitFileName[0];         // 파일이름
+    // var sFileExtension = arSplitFileName[1];     // 확장자
+    
+    // var sFileName_new = sFileName.replace(/\s+/g, '%20'); //공백을 '%'문자로 치환해주어야 사진이 제대로 출력됨.
+    // var img_url_fin = sFileName_new+"."+sFileExtension;
+
+
+
     var reply = {
         "attachment": {
             "type": "template",
@@ -13,19 +29,19 @@ genericFBT: function genericFBT(genericImgUrl, genericTitile, genericSubtitile, 
                 "elements":[
                    {
                     "title": title,
-                    "image_url": imgurl,
+                    "image_url": refineUrl(imgurl),
                     "subtitle": subtitle,
                     "buttons":[
                         {
                             "type":"web_url",
                             "url": buttonurl,
                             "title":buttonname
-                          }    
-                        // {
-                        //         "type":"postback",
-                        //         "title":"안녕",
-                        //         "payload":"안녕"
-                        //     }
+                          }
+                          // ,{
+                          //       "type":"postback",
+                          //       "title":"안녕",
+                          //       "payload":"안녕"
+                          //   }
                         ]
                     }]      
                   
@@ -38,13 +54,52 @@ genericFBT: function genericFBT(genericImgUrl, genericTitile, genericSubtitile, 
   return genericTemplate(genericImgUrl, genericTitile, genericSubtitile, genericButtonName, genericButtonURL );
 },
 
+genericTwoFBT: function genericTwoFBT(genericImgUrl, genericTitile, genericSubtitile, genericButtonName, genericButtonURL, genericButtonName2nd ){
+  //입력된 부분을 generic 템플릿 형식으로 변환
+
+  function genericTwoTemplate(imgurl, title, subtitle, buttonname, buttonurl, buttonname2nd){
+
+    var reply = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type":"generic",
+                "elements":[
+                   {
+                    "title": title,
+                    "image_url": refineUrl(imgurl),
+                    "subtitle": subtitle,
+                    "buttons":[
+                        {
+                            "type":"web_url",
+                            "url": buttonurl,
+                            "title":buttonname
+                          },{
+                            "type":"postback",
+                            "title": buttonname2nd,
+                            "payload": title
+                          }    
+                        ]
+                    }]      
+                  
+              }
+        }
+    }
+  return reply
+}
+  
+  return genericTwoTemplate(genericImgUrl, genericTitile, genericSubtitile, genericButtonName, genericButtonURL, genericButtonName2nd );
+},
+
+
 genrInnerFBT: function genrInnerFBT(genericImgUrl, genericTitile, genericSubtitile, genericButtonName, genericButtonURL ){
     //입력된 부분을 generic 템플릿 형식으로 변환
   
     function genericInnerTemplate(imgurl, title, subtitle, buttonname, buttonurl){
+      
       var reply = {                      
                       "title": title,
-                      "image_url": imgurl,
+                      "image_url": refineUrl(imgurl),
                       "subtitle": subtitle,
                       "buttons":[
                           {
@@ -60,6 +115,51 @@ genrInnerFBT: function genrInnerFBT(genericImgUrl, genericTitile, genericSubtiti
     
     return genericInnerTemplate(genericImgUrl, genericTitile, genericSubtitile, genericButtonName, genericButtonURL );
   },
+
+
+  //url버튼 1개, postback버튼 1개 조합
+  genrInnerTwoFBT: function genrInnerTwoFBT(genericImgUrl, genericTitile, genericSubtitile, genericButtonName, genericButtonURL, genericButtonName2nd){
+    //입력된 부분을 generic 템플릿 형식으로 변환
+  
+    function genericInnerTwoTemplate(imgurl, title, subtitle, buttonname, buttonurl, buttonname2nd){
+      
+      var reply = {                      
+                      "title": title,
+                      "image_url": refineUrl(imgurl),
+                      "subtitle": subtitle,
+                      // "default_action": {
+                      //   "type": "web_url",
+                      //   "url": buttonurl,
+                      //   "messenger_extensions": true,
+                      //   "webview_height_ratio": "tall",
+                      //   "fallback_url": buttonurl
+                      // },
+                      "buttons":[
+                            {
+                              "type":"web_url",
+                              "url": buttonurl,
+                              "title":buttonname
+                            },{
+                              "type":"postback",
+                              "title": buttonname2nd,
+                              "payload": title
+                            }    
+ 
+                          ]}
+      
+    return reply
+  }
+    
+    return genericInnerTwoTemplate(genericImgUrl, genericTitile, genericSubtitile, genericButtonName, genericButtonURL, genericButtonName2nd);
+  },
+
+
+  ////
+
+
+
+
+
 
   cardFBT: function cardFBT(input){
     //입력된 부분을 card 템플릿 형식으로 변환 
@@ -136,6 +236,15 @@ listInnerFBT: function listInnerFBT(Question, Answer, url){
   return reply
 }
         
+}
+
+//파일url경로 정제 작업
+//파일명에 띄어쓰기가 포함된 사진이 있는 것에 주의해야 함 
+//공백 문자 아스키코드 = '%20'
+function refineUrl(input_url){
+  var fileNameRefined = path.basename(input_url, path.extname(input_url)).replace(/\s+/g, '%20');
+  var final_img_url = path.dirname(input_url) + "/" + fileNameRefined + path.extname(input_url);
+  return final_img_url;
 }
 
 
--- registry.js
+++ registry.js
@@ -3,7 +3,7 @@ module.exports = {
     // FinancialBot
     'MedicineRetrieval' : require('./Medicine/medicine_retrieval'),
     'FAQRetrieval' : require('./FAQ/FAQ_retrieval'),
-    'FindHospitalRetrieval' : require('./Hospital/FindHospital_retrieval')
-    
+    'FindHospitalRetrieval' : require('./Hospital/FindHospital_retrieval'),
+    'ProfessorInDeptRetrieval' : require('./Hospital/ProfessorInDept_retrieval')
   }
 };
